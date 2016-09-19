fddd0bda3fadd92365ff3641636ca1e210b74329
Don Denton <don@happycollision.com>
2016-09-19 16:11:23 -0500

    Add slugify helper...

--- app/lib/helper-functions.js
+++ app/lib/helper-functions.js
@@ -7,6 +7,10 @@ define([
       return str.replace(/\w\S*/g, function(txt){
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
       });
+    },
+    
+    slugify: function (str) {
+      return str.replace(/\s*/g, '-');
     }
 
   };
